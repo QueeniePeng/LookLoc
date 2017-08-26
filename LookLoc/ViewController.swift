cde7e701e339ad454a4aaeb5728c7f3b20261b98
QueeniePeng <pengqueenie@gmail.com>
2017-08-26 11:28:17 -0500

    Initial Commit

---
+++ LookLoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LookLoc
+//
+//  Created by pengQueenie on 2017/8/26.
+//  Copyright © 2017年 pengQueenie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
